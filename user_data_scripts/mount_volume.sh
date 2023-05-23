8c608ff0e6c5424b9d8afdfcfd1c6cd9af62b561
Henry Skiba <henry.skiba@frgrisk.com>
2023-05-23 16:19:11 +0800

    Fix hardcoded device name in volume mount

--- user_data_scripts/mount_volume.sh
+++ user_data_scripts/mount_volume.sh
@@ -3,5 +3,5 @@ set -e
 set -x
 mkfs -t xfs ${device}
 mkdir -p ${mount_point}
-echo "UUID=$(blkid -o value -s UUID /dev/nvme1n1) ${mount_point} xfs defaults,nofail 0 2" | tee -a /etc/fstab
+echo "UUID=$(blkid -o value -s UUID ${device}) ${mount_point} xfs defaults,nofail 0 2" | tee -a /etc/fstab
 mount -a
