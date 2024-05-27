38043008f8e2e9307d8f9e83fbacc78150b756bd
Henry Skiba <henry.skiba@frgrisk.com>
2024-05-27 11:53:53 +0800

    add `required_providers` block

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+}
+
 locals {
   tag_name = coalesce(var.tag_name, var.hostname)
 
